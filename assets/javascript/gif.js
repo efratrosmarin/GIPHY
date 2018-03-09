5c071694b2661780f1a27dbb782f9ac569c3626b
Efrat Rosmarin <efratedit@gmail.com>
2018-03-08 20:50:03 -0500

    add your own show button

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -14,7 +14,8 @@ $("button").on("click", function() {
     })
       // After the data comes back from the API
       .then(function(response) {
-          // console.log(response)
+        
+          console.log(response)
         // Storing an array of results in the results variable
         var results = response.data;
         // Looping over every result item
@@ -43,5 +44,74 @@ $("button").on("click", function() {
   });
   
 
+
+
+  var persons = [];
+      // displaypersonInfo function re-renders the HTML to display the appropriate content
+      function displaypersonInfo() {
+        var person = $(this).attr("data-person");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      person + "&api_key=dc6zaTOxFJmzC&limit=10";
+        // Creating an AJAX call for the specific person button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+
+          console.log(response);
+          // Creating a div to hold the person
+          var personDiv = $("<div class='person'>");
+          // Storing the rating data
+          var rating = response.rating;
+          // Creating an element to have the rating displayed
+          var pOne = $("<p>").text(rating);
+          // Displaying the rating
+          personDiv.append(pOne);
+          // Storing the release year
+          
+          // Retrieving the URL for the image
+          var imgURL = response.images;
+          // Creating an element to hold the image
+          var images = $("<img>").attr("src", imgURL);
+          // Appending the image
+          personDiv.append(images);
+          // Putting the entire person above the previous persons
+          $("#persons-view").prepend(personDiv);
+        });
+      }
+      // Function for displaying person data
+      function renderButtons() {
+        // Deleting the persons prior to adding new persons
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#buttons-view").empty();
+        // Looping through the array of persons
+        for (var i = 0; i < persons.length; i++) {
+          // Then dynamicaly generating buttons for each person in the array
+          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+          var a = $("<button>");
+          // Adding a class of person-btn to our button
+          a.addClass("person-btn");
+          // Adding a data-attribute
+          a.attr("data-person", persons[i]);
+          // Providing the initial button text
+          a.text(persons[i]);
+          // Adding the button to the buttons-view div
+          $("#buttons-view").append(a);
+        }
+      }
+      // This function handles events where a person button is clicked
+      $("#add-person").on("click", function(event) {
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var person = $("#person-input").val().trim();
+        // Adding person from the textbox to our array
+        persons.push(person);
+        // Calling renderButtons which handles the processing of our person array
+        renderButtons();
+      });
+      // Adding a click event listener to all elements with a class of "person-btn"
+      $(document).on("click", ".person-btn", displaypersonInfo);
+      // Calling the renderButtons function to display the intial buttons
+      renderButtons();
 
 });//ends document

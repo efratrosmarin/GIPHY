c01c46b21ed8376a156d33aae405e4d6f25392a4
Efrat Rosmarin <efratedit@gmail.com>
2018-03-11 15:58:38 -0400

    still image

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -76,7 +76,45 @@ $(document).ready(function () {
 
 
 
-        var imgTv = response.data[i].images.original.url;
+//         var animated = results[i].images.fixed_height.url;
+//         var still = results[i].images.fixed_height_still.url;
+
+//         var personImage = $("<img>");
+//         personImage.attr("src", still);
+//         personImage.attr("data-still", still);
+//         personImage.attr("data-animate", animated);
+//         personImage.attr("data-state", "still");
+//         personImage.addClass("person-image");
+
+//         personDiv.append(p);
+//         personDiv.append(personImage);
+
+//         $("#gifs-appear-here").append(personDiv);
+//       }
+//     });
+// });
+
+// $(document).on("click", ".person-image", function() {
+
+//   var state = $(this).attr("data-state");
+
+//   if (state === "still") {
+//     $(this).attr("src", $(this).attr("data-animate"));
+//     $(this).attr("data-state", "animate");
+//   }
+//   else {
+//     $(this).attr("src", $(this).attr("data-still"));
+//     $(this).attr("data-state", "still");
+//   }
+// });
+
+
+
+
+
+
+
+        var imgTv = response.data[i].images.fixed_height_still.url;
         // Creating an element to hold the image
         var images = $("<img>").attr("src", imgTv);
         // Appending the image
